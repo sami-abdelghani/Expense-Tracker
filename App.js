56f59869002af7b3083c06db0519b4e5e85ed9df
sami-abdelghani <62582814+sami-abdelghani@users.noreply.github.com>
2021-07-14 02:43:38 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ExpenseItem from './components/ExpenseItem';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Let's get Started!</h2>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
